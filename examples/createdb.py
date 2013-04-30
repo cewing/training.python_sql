d4266e0074a14a06ca55ab4c55f4f3e6ce1ada63
cewing <cris@crisewing.com>
2013-04-30 15:48:52 -0700

    set original createdb.py to starting point.

--- examples/createdb.py
+++ examples/createdb.py
@@ -3,11 +3,14 @@ import sqlite3
 
 DB_FILENAME = 'books.db'
 DB_IS_NEW = not os.path.exists(DB_FILENAME)
-conn =  sqlite3.connect(DB_FILENAME)
 
-if DB_IS_NEW:
-    print 'Need to create database and schema'
-else:
-    print 'Database exists, assume schema does, too.'
+def main():
+    conn =  sqlite3.connect(DB_FILENAME)
+    if DB_IS_NEW:
+        print 'Need to create database and schema'
+    else:
+        print 'Database exists, assume schema does, too.'
+    conn.close()
 
-conn.close()
+if __name__ == '__main__':
+    main()
